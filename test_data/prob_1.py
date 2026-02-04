b63b1e3fe9c323f03cbae09f2084959f97f19213
Suraj (Big-chillz) <bigchill9949749467@gmail.com>
2026-02-04 22:18:51 +0530

    day 6

--- test_data/prob_1.py
+++ test_data/prob_1.py
@@ -7,28 +7,23 @@ np.random.seed(52)
 def toss_coin(n):
     return np.random.choice(['Heads', 'Tails'], size=n)
 
-
+"""
 a=int(input("enter number of tosses: "))
 results = toss_coin(a)
 print(results)
 
 heads_ratio = np.sum(results == 'Heads')/a
-print(heads_ratio)
-
+print(heads_ratio)"""
+"""
 ratios = []
 for n in range(10,1000,10): #10,20,30,40 ..... 980,990,999
     resulting_coin = toss_coin(n)
     print(f"{resulting_coin} for n={n}")
     r = np.sum(resulting_coin == 'Heads')/n
     ratios.append(r)
+"""
 
-print(ratios)
-print(len(ratios))
-
-plt.plot(ratios)
-plt.axhline(0.5,color='red')
-plt.xlabel("Number of tosses")
-plt.ylabel("Ratio of Heads")
-plt.show()
-
-
+prob = 0.9
+trials = np.random.rand(20) < prob
+print(trials)
+print("failures despite high confidence", np.sum(trials==False))
