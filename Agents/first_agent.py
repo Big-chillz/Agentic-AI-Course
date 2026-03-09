4ba214709c118c632b995b5dbc904d7153e83df8
Suraj (Big-chillz) <bigchill9949749467@gmail.com>
2026-03-09 21:47:39 +0530

    day 25

--- Agents/first_agent.py
+++ Agents/first_agent.py
@@ -1,3 +1,4 @@
+# first execution will be importing necessary libraries and defining the knowledge base, which is a list of sentences containing information about AI agents, machine learning, debugging, artificial intelligence, and Python programming language. We will use the SentenceTransformer model to encode the knowledge base and the query for similarity comparison. 
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
@@ -14,30 +15,36 @@ KNowledgeBase = [
 model = SentenceTransformer('all-MiniLM-L6-v2')
 knowledge_in_embeddings = model.encode(KNowledgeBase)
 
+# sixth execution
 def search_knowledge_base(query) : #retrival tool, we will use cosine similarity to find the most relevant information from the knowledge base based on the query. We will encode the query and compare it with the pre-encoded knowledge base to find the best match.
     query_embedding = model.encode([query])
     score = cosine_similarity(query_embedding, knowledge_in_embeddings) #query_embedding is 1x384 and knowledge_in_embeddings is 5x384, so the result will be 1x5 => 1x384 . 384x5 => 1x5 = [0.8, 0.6, 0.4, 0.2, 0.1] -> examples
     best_match_index = np.argmax(score)
     return KNowledgeBase[best_match_index]
 
+# fourth execution
 def goal_agent(goal) : 
     print("\n Agent thinking about Goal:", goal)
     query = goal
     return query
 
+# fifth execution
 def action_agent(query) :
     print("\n Agent searching the knowledge base for query:", query)
     return search_knowledge_base(query)
 
+# seventh execution
 def observe_agent(result) :
     print("\n Agent observed the result:", result)
 
 
+# third execution
 def orchestrator_agent(goal) :
-    query = goal_agent(goal)
-    result = action_agent(query)
+    query_agent = goal_agent(goal)
+    result = action_agent(query_agent)
     observe_agent(result)
    
-if __name__ == "__main__":
+# second execution
+if __name__ == "__main__": 
     goal = "What can AI agents do?"
     orchestrator_agent(goal)
